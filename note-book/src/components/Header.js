83d77eeee01d6735eff9ec0ab57fffe970e131bd
HiepThuongTiTus <thuongthithuhiep2452000123@gmail.com>
2020-12-30 11:25:08 +0700

    DoanCuoiki

---
+++ note-book/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+function Header() {
+
+    return ( //HTML vs JSX
+        <nav className="navbar navbar-expand-lg myheader" >
+            <a class="title">OTTONOTE BOOK</a>
+        </nav>
+    )
+}
+
+
+
+export default Header;
